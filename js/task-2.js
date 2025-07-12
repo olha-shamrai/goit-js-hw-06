e108e702b4644f71e28ef3c97a06ae05cf1ff431
Olha Shamrai <olga.moryacka91@gmail.com>
2025-07-13 01:06:16 +0300

    add task 2

--- js/task-2.js
+++ js/task-2.js
@@ -9,6 +9,28 @@
 
 
 
+class Storage {
+    #items = [];
+
+    constructor(params) {
+        this.#items = params;
+    }
+
+    getItems() {
+        return this.#items;
+    }
+
+    addItem(newItem) {
+        this.#items.push(newItem);
+    }
+
+    removeItem(itemToRemove) {
+        this.#items = this.#items.filter(item => item !== itemToRemove);
+    }
+}
+
+
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
